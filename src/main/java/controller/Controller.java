fbd85b620fb10f1499e752c59d88f5869f247340
Renato <renato.silva15948@gmail.com>
2022-08-26 18:26:53 -0300

    configurando style

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Controller
  */
-@WebServlet("/Controller")
+@WebServlet(urlPatterns = {"/Controller", "/main"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
