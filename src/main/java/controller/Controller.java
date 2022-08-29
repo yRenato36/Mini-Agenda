815d967aec02b6c0f0c6d67c52b0018c1dc68911
Renato <renato.silva15948@gmail.com>
2022-08-29 19:42:05 -0300

    criando conexão com o banco de dados

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -6,28 +6,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.DAO; 
 
-/**
- * Servlet implementation class Controller
- */
 @WebServlet(urlPatterns = {"/Controller", "/main"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	DAO	dao = new DAO();  
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public Controller() {
+		super();
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//testando conexão
+		//dao.testeConexao();
+
+		String action = request.getServletPath();
+		System.out.println(action);
+
+		if(action.equals("/main")) {
+			contatos(request,response);
+		}
+	}
+	//listar contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+	}
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,42 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	/*Módulo de conexão*/
+	/*Parâmetros de conexão com o banco de dados*/
+	private String driver = "com.mysql.cj.jdbc.Driver"; //chamando o driver para conectar ao BD
+	
+	//string de conexão
+	private String url = 
+			"jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	//user e senha do banco de dados
+	private String user = "root";
+	private String password = "123456";
+	
+	//Métodos de conexão
+	//classe que está dentro do driver de conexão
+	private Connection conectar() {
+		Connection con = null; // estabelecerá uma conexão com o banco de dados
+		
+		try {
+			Class.forName(driver); //lê o driver do BD e busca a variável driver
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	// teste de conexão
+	/*public void testeConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}*/
 }
