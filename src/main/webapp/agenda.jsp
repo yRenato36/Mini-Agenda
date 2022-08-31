77d0a93528d5808293690d251e2235b53c6a8f2c
Renato <renato.silva15948@gmail.com>
2022-08-31 18:21:12 -0300

    Validação de campos com js + nova tela + enviando form para o controller

--- src/main/webapp/agenda.jsp
+++ src/main/webapp/agenda.jsp
@@ -10,6 +10,6 @@
 </head>
 <body>
 	<h1>Agenda de Contatos</h1>
-	<a href="" class="Botao1">Novo contato</a>
+	<a href="novo.html" class="Botao1">Novo contato</a>
 </body>
 </html>
---
+++ src/main/webapp/scripts/validador.js
@@ -0,0 +1,21 @@
+/**
+ * Validação de formularios
+ * Autor: Renato Santana
+ */
+
+function validar() {
+    let nome = frmContato.nome.value //variável que recebe do form frmContato campo nome o valor
+    let fone = frmContato.fone.value
+    
+    if(nome === "") {
+		alert("Preencha o campo Nome")
+		frmContato.nome.focus() //posiciona o cursor em cima do campo nome
+		return false
+	} else if (fone === "") {
+		alert("Preencha o campo Fone")
+		frmContato.fone.focus() //posiciona o cursor em cima do campo Fone
+		return false
+	} else {
+		document.forms["frmContato"].submit() //envia o formulário dos dados para a camada controller
+	}
+}
